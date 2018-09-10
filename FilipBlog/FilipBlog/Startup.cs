9464df48b800795b9ee6c63a9abe6f94608a9fdb
filipmarkoski45 <filip.markoski45@gmail.com>
2018-09-11 00:56:12 +0200

    Initialized ASP.NET project

---
+++ FilipBlog/FilipBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FilipBlog.Startup))]
+namespace FilipBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
